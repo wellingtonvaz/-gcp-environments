6e3750f406c81e282ba9d2cd1791248cd9305bef
Gustavo Leon <gustavo.leon@uniktilabs.com.br>
2022-10-26 14:52:48 -0300

    ajustes realizados novamente

--- envs/development/main.tf
+++ envs/development/main.tf
@@ -14,11 +14,41 @@
  * limitations under the License.
  */
 
-module "env" {
-  source = "../../modules/env_baseline"
+#module "env" {
+#  source = "../../modules/env_baseline"
+#
+#  env                        = "development"
+#  environment_code           = "d"
+#  monitoring_workspace_users = var.monitoring_workspace_users
+#  remote_state_bucket        = var.remote_state_bucket
+#}
 
-  env                        = "development"
-  environment_code           = "d"
-  monitoring_workspace_users = var.monitoring_workspace_users
-  remote_state_bucket        = var.remote_state_bucket
+
+ocals {
+  org_id          = data.terraform_remote_state.bootstrap.outputs.common_config.org_id
+  parent_folder   = data.terraform_remote_state.bootstrap.outputs.common_config.parent_folder
+  parent          = data.terraform_remote_state.bootstrap.outputs.common_config.parent_id
+  billing_account = data.terraform_remote_state.bootstrap.outputs.common_config.billing_account
+  default_region  = data.terraform_remote_state.bootstrap.outputs.common_config.default_region
+  project_prefix  = data.terraform_remote_state.bootstrap.outputs.common_config.project_prefix
+  folder_prefix   = data.terraform_remote_state.bootstrap.outputs.common_config.folder_prefix
+  tags            = data.terraform_remote_state.org.outputs.tags
+}
+
+data "terraform_remote_state" "bootstrap" {
+  backend = "gcs"
+
+  config = {
+    bucket = var.remote_state_bucket
+    prefix = "terraform/bootstrap/state"
+  }
+}
+
+data "terraform_remote_state" "org" {
+  backend = "gcs"
+
+  config = {
+    bucket = var.remote_state_bucket
+    prefix = "terraform/org/state"
+  }
 }
